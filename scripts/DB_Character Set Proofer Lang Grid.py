d469a1b30e1a829a055c68455f1fcdb5228091ba
Peter Laxalt <peter@laxa.lt>
2024-03-19 01:34:39 -0400

    more hirigana:

---
+++ scripts/DB_Character Set Proofer Lang Grid.py
@@ -0,0 +1,200 @@
+'''Character Set Proofer'''
+
+from glyphNameFormatter.reader import *
+
+f = CurrentFont()
+allUnis = list(uni2name.keys())
+
+######################################################
+
+### config
+
+# colors
+def setBackgroundColor():
+    fill(0,0,0, 1)
+    rect(0, 0, pageWidth, pageHeight)
+
+def setForegroundColor():
+    fill(255,255,255, 1)
+
+# main variables
+boxHeight = 70
+boxPaddingX = 20
+boxPaddingY = 20
+margin = 30
+groupFontSize = 16
+groupFontFamily = 'Dank Mono'
+
+# unicode groups
+unicodeGroups = True
+unicodeFilters = ['Hiragana']
+
+# return all glyphs at end
+returnAllGlyphs = False
+
+# define page size
+size('TabloidLandscape')
+
+######################################################
+
+# set page size
+pageWidth, pageHeight = width(), height()
+
+# calculate initial positions
+x = margin
+y = pageHeight - margin - boxHeight
+
+# set initial position
+xBox, yBox = x, y
+
+# calculate scale
+s = float(boxHeight) / f.info.unitsPerEm
+
+# background
+setBackgroundColor()
+
+# draw groups
+
+uniRangeGroups = []
+
+for i, glyphName in enumerate(f.glyphOrder):
+    if glyphName == 'space':
+        continue
+    
+    uniValue = n2u(glyphName)
+    uniRange = u2r(uniValue)
+    
+    if uniRange not in uniRangeGroups:
+        uniRangeGroups.append(uniRange)
+        
+print('‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡')
+print('‡‡‡ Current groups: ', uniRangeGroups)
+print('‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡')
+
+def filter_arr(arr, filters):
+    return [x for x in arr if x in filters]
+        
+if unicodeFilters:
+    _originalGroups = uniRangeGroups
+    uniRangeGroups = filter_arr(_originalGroups, unicodeFilters)
+    print('')
+    print('‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡')
+    print('‡‡‡ Filters: ', unicodeFilters)
+    print('‡‡‡ Filtered groups: ', uniRangeGroups)
+    print('‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡')
+    
+
+if unicodeGroups and uniRangeGroups:
+    for i, groupName in enumerate(uniRangeGroups):
+        print(i)
+            
+        if groupName:
+
+            # new page
+            if i != 0:
+                newPage(pageWidth, pageHeight)
+                setBackgroundColor()
+            
+            # set position 
+            xBox, yBox = x, y - (groupFontSize + margin)
+            
+            # set colors
+            setForegroundColor()
+            font(groupFontFamily, groupFontSize)
+            text(groupName, (x, pageHeight - (margin + groupFontSize)))
+            
+            # loop through glyphs
+            for i, glyphName in enumerate(f.glyphOrder):
+                
+                if glyphName == 'space':
+                    continue
+                
+                # check glyph
+                uniValue = n2u(glyphName)
+                uniRange = u2r(uniValue)
+                
+                # draw glyph
+                if groupName == uniRange:
+                    g = f[glyphName]
+                    boxWidth = g.width*s
+                    # boxWidth = boxHeight
+
+                    # jump to next line
+                    if xBox + boxWidth >= pageWidth - margin:
+                        xBox = x
+                        yBox -= boxHeight + boxPaddingY
+                        # jump to next page
+                        if yBox < margin:
+                            yBox = y
+                            newPage(pageWidth, pageHeight)
+                            setBackgroundColor()
+                            setForegroundColor()
+                            font(groupFontFamily, groupFontSize)
+                            text(groupName + ' (Continued)', (x, pageHeight - (margin + groupFontSize)))
+                            xBox, yBox = x, y - (groupFontSize + margin)
+
+                    # draw glyph cell
+                    # stroke(0, 0.5, 1)
+                    fill(None)
+                    rect(xBox, yBox, boxWidth, boxHeight)
+
+                    # draw glyph
+                    xGlyph = xBox
+                    yGlyph = yBox - f.info.descender*s
+                    save()
+                    translate(xGlyph, yGlyph)
+                    setForegroundColor()
+                    stroke(None)
+                    scale(s)
+                    drawGlyph(g)
+                    restore()
+
+                    # move to next glyph
+                    xBox += boxWidth + boxPaddingX
+
+
+######################################
+
+# draw all glyphs
+
+if returnAllGlyphs:
+    newPage(pageWidth, pageHeight)
+    setBackgroundColor()
+    xBox, yBox = x, y
+
+    for i, glyphName in enumerate(f.glyphOrder):
+    
+        if glyphName == 'space':
+            continue
+
+        g = f[glyphName]
+        boxWidth = g.width*s
+
+        # jump to next line
+        if xBox + boxWidth >= pageWidth - margin:
+            xBox = x
+            yBox -= boxHeight + boxPaddingY
+            # jump to next page
+            if yBox < margin:
+                yBox = y
+                newPage(pageWidth, pageHeight)
+                setBackgroundColor()
+
+        # draw glyph cell
+        # stroke(0, 0.5, 1)
+        fill(None)
+        rect(xBox, yBox, boxWidth, boxHeight)
+
+        # draw glyph
+        xGlyph = xBox
+        yGlyph = yBox - f.info.descender*s
+        save()
+        translate(xGlyph, yGlyph)
+        setForegroundColor()
+        stroke(None)
+        scale(s)
+        drawGlyph(g)
+        restore()
+
+        # move to next glyph
+        xBox += boxWidth + boxPaddingX
--- scripts/DB_Character Set Proofer.py
+++ scripts/DB_Character Set Proofer.py
@@ -2,9 +2,17 @@
 
 f = CurrentFont()
 
+# colors
+def setBackgroundColor():
+    fill(0,0,0, 1)
+    rect(0, 0, pageWidth, pageHeight)
+
+def setForegroundColor():
+    fill(255,255,255, 1)
+
 # main variables
 boxHeight = 70
-boxPaddingX = 0
+boxPaddingX = 20
 boxPaddingY = 20
 margin = 30
 
@@ -12,19 +20,27 @@ margin = 30
 s = float(boxHeight) / f.info.unitsPerEm
 
 # define page size
-size('A4Landscape')
+size('TabloidLandscape')
 pageWidth, pageHeight = width(), height()
 
 # calculate initial positions
 x = margin
 y = pageHeight - margin - boxHeight
 
+# background
+setBackgroundColor()
+
 # draw glyphs
 xBox, yBox = x, y
+
 for i, glyphName in enumerate(f.glyphOrder):
+    
+    if glyphName == 'space':
+        continue
 
     g = f[glyphName]
     boxWidth = g.width*s
+    # boxWidth = boxHeight
 
     # jump to next line
     if xBox + boxWidth >= pageWidth - margin:
@@ -34,9 +50,10 @@ for i, glyphName in enumerate(f.glyphOrder):
         if yBox < margin:
             yBox = y
             newPage(pageWidth, pageHeight)
+            setBackgroundColor()
 
     # draw glyph cell
-    stroke(0, 0.5, 1)
+    # stroke(0, 0.5, 1)
     fill(None)
     rect(xBox, yBox, boxWidth, boxHeight)
 
@@ -45,7 +62,7 @@ for i, glyphName in enumerate(f.glyphOrder):
     yGlyph = yBox - f.info.descender*s
     save()
     translate(xGlyph, yGlyph)
-    fill(0)
+    setForegroundColor()
     stroke(None)
     scale(s)
     drawGlyph(g)
